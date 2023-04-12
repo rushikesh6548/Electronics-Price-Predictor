4e0516e1bd4823a929f16f4142a48d68e0be1a84
Rushikesh Wakhare <rrushi6548@gmail.com>
2023-04-12 15:48:45 +0530

    Class changes in dataingestionconfig

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,9 +6,12 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from dataclasses import dataclass
 
-@dataclass()
+
+
+
 class DataIngestionConfig:
-    train_data_path : str = os.path.join('artifact','train.csv')
+    def __init__(self):
+        train_data_path : str = os.path.join('artifact','train.csv')
+        
 
