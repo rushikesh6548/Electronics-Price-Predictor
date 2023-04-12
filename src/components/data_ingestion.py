3284328aad54214bfa32b1c0683e5bd314fd6095
Rushikesh Wakhare <rrushi6548@gmail.com>
2023-04-12 15:41:52 +0530

    EDA on Laptop Dataset

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,2 +1,14 @@
 from src.exception import CustomException
 from src.logger import logging
+import os
+import sys
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from dataclasses import dataclass
+
+@dataclass()
+class DataIngestionConfig:
+    train_data_path : str = os.path.join('artifact','train.csv')
+
